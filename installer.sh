26309e1852016b4ce7445698f5dc1670c3a96ab1
jonathan <celluloid-demon@users.noreply.github.com>
2023-12-09 22:30:53 -0800

    hah

--- installer.sh
+++ installer.sh
@@ -3,18 +3,5 @@
 # Exit on error
 set -e
 
-# Declare vars
-source=./home/*
-destination="$HOME/.shared-config-nix"
-
-# Make install dir if it doesn't exist
-mkdir -p "$destination"
-
-# Remove previous contents
-rm -r "$destination"/*
-
-# Copy new contents
-cp -r $source "$destination"
-
 # Prompt user for follow-up actions
-echo "Files copied to $destination. Now add to PATH in bash profile. Go away."
+echo "Just add symbolic links to directories here. Go away."
